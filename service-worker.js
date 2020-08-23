f13b19f2909b54851732ef82bb141014ab6a15bd
AhmadBilalAmjad <ahmad.bilal.amjad@outlook.com>
2020-08-23 11:30:38 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6309a7112f734eb02a43b8439ab1f1b8.js"
+  "/precache-manifest.8fed84bebcf0a9442a2d30976dcc3066.js"
 );
 
 self.addEventListener('message', (event) => {
