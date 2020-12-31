6428740a6137320f275e39598e4591d745ad7ee2
AhmadBilalAmjad <ahmad.bilal.amjad@outlook.com>
2020-12-31 18:09:03 +0500

    Updates

---
+++ precache-manifest.b39ea74b2c671a75b3a4bb71c0df959f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a29cbb1ab1aa80d65d0bdba0abf26c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9eec8057fa3ff3ab9e",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "8611e38232788b1e1793",
+    "url": "/static/css/main.6b038bfc.chunk.css"
+  },
+  {
+    "revision": "db9eec8057fa3ff3ab9e",
+    "url": "/static/js/2.28fe241a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.28fe241a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8611e38232788b1e1793",
+    "url": "/static/js/main.fc593c2b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "03d7cee29f2d6da5fff670277c1fc2b6",
+    "url": "/static/media/background.03d7cee2.jpg"
+  }
+]);
