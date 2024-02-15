baff0ea26d44d3859f6556f209e1978a79f2a8d7
Ahmad Bilal Amjad <47005622+AhmadBilalAmjad@users.noreply.github.com>
2024-02-15 20:01:27 +0500

    redirect added to new next js website

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/projects">
-          <Projects />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+        <Redirect to="https://ahmadbilalamjad.vercel.app/" />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/projects">
+              <Projects />
+            </Route>
+          </Switch>
+        </Router>
+    </>
   );
 }
